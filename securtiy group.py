e33e9bdc7e65c4b758bebd62cdd2d28b218ac131
lakshmicharan-star <53990763+lakshmicharan-star@users.noreply.github.com>
2020-02-12 15:18:41 +0000

    Delete securtiy group.py

--- securtiy group.py
+++
@@ -1,4 +0,0 @@
-import boto3
-ec2 = boto3.client('ec2')
-response = ec2.create_security_group(GroupName='testgroup2',Description='testme')
-ec2.authorize_security_group_ingress(GroupId=response['GroupId'],IpProtocol="tcp",CidrIp="0.0.0.0/0",FromPort=80,ToPort=80)
